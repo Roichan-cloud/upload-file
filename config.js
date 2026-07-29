f637eb9390f9cc5b91ed528149e2ac3ffb5a4cc6
Frre-Cloud <roichannoob@gmail.com>
2026-07-30 05:09:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@
    JANGAN PERNAH memakai Service Role Key di sini — hanya Anon/Public Key.
    ========================================================================== */
 const CONFIG = {
-  SUPABASE_URL: "https://YOUR-PROJECT-REF.supabase.co", // URL project Supabase
-  SUPABASE_ANON_KEY: "YOUR-ANON-PUBLIC-KEY",             // Anon/public key (aman untuk frontend)
+  SUPABASE_URL: "https://iofafbpczagpdpiufxxz.supabase.co", // URL project Supabase
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlvZmFmYnBjemFncGRwaXVmeHh6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUzMjQwMzgsImV4cCI6MjEwMDkwMDAzOH0.GEuFVUtv7XRiDFKWY-6US8kb59swcYr9o_JDS-C3i4c",             // Anon/public key (aman untuk frontend)
   STORAGE_BUCKET: "uploads",                             // Nama bucket Storage (harus public)
   MAX_FILE_SIZE: 100 * 1024 * 1024                        // Ukuran file maksimum (default 100MB) dalam byte
 };
