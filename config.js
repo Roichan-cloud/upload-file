298740ed8a2554b9752651c719b60e17c8af1da7
Frre-Cloud <roichannoob@gmail.com>
2026-08-11 13:31:17 +0700

    Add .fbx to allowed file extensions

--- config.js
+++ config.js
@@ -9,5 +9,5 @@ const CONFIG = {
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlvZmFmYnBjemFncGRwaXVmeHh6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUzMjQwMzgsImV4cCI6MjEwMDkwMDAzOH0.GEuFVUtv7XRiDFKWY-6US8kb59swcYr9o_JDS-C3i4c",             // Anon/public key (aman untuk frontend)
   STORAGE_BUCKET: "uploads",                             // Nama bucket Storage (harus public)
   MAX_FILE_SIZE: 100 * 1024 * 1024,                       // Ukuran file maksimum (default 100MB) dalam byte
-  ALLOWED_EXTENSIONS: [".json", ".mp3", ".mp4"]                           // Ekstensi file yang diizinkan diunggah (ubah di sini kalau perlu tipe lain)
+  ALLOWED_EXTENSIONS: [".json", ".mp3", ".mp4", ".fbx"]                           // Ekstensi file yang diizinkan diunggah (ubah di sini kalau perlu tipe lain)
 };
